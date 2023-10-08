b6242120f312ca95496d1fe5d134c13d344a439b
Vleforn <95494939+Vleforn@users.noreply.github.com>
2023-10-08 14:31:58 +0600

    fix: bubble sort

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -6,16 +6,16 @@ my_list2 = [1, 2, 3, 4, 5, 7, 6]
 
 def bubble_sort(l: List[int]) -> List[int]:
     length = len(l)
-    counter = 0
+    #counter = 0
     for i in range(length - 1):
         swapped = False
         for j in range(length - 1 - i):
             if l[j] > l[j+1]:
                 l[j], l[j+1] = l[j+1], l[j]
                 swapped = True
-        counter += 1
+        #counter += 1
         if swapped == False:
             break
-    return l, counter
+    return l#, counter
 
 print(bubble_sort(my_list2))
